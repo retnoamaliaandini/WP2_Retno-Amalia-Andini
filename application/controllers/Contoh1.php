0b579b34f471c686dffe7180976471752a60f824
retnoamaliaandini <19200582@bsi.ac.id>
2021-09-14 21:38:31 +0700

    installasi codeigniter dan membuat latihan contoh 1 pada controller

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h2>Perkenalkan</h2>";
+        echo "Nama saya Retno Amalia Andini kelas 19.3E.07
+            Saya tinggal di daerah Cakung
+            olahraga kesukaan saya adalah bulu tangkis";
+    }
+}
+
