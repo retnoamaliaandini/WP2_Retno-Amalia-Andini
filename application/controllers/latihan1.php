32762ef3aa83caa8a513f9a67592b9a02c9ea920
retnoamaliaandini <19200582@bsi.ac.id>
2021-09-30 13:21:12 +0700

    menambahkan MVC dan menghilangkan index.php

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,21 @@
+<?php
+class Latihan1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<b><h2> Selamat datang ... selamat belajar web programing </h2></b>";
+    }
+    public function penjumlahan($n1,$n2)
+    {
+        // $this->load->model('Model_latihan1');
+        // $hasil=$this->Model_latihan1->jumlah($n1,$n2);
+        // echo "Hasil penjumlahan dari " . $n1 . " + " . $n2 . " = " .$hasil;
+
+        $this->load->model('Model_latihan1');
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1,$n2);
+
+        $this->load->view('view-latihan1',$data);
+    }
+}
---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    Hello Friends.. let's learn web programming <br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
+</body>
+</html>
